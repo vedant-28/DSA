ef27eb65d0f78f29495abe7633630cb29cd82e59
vedant <vedant10vaidya@gmail.com>
2026-01-18 20:25:49 +0900

    DSA Practice | Arrays

---
+++ Arrays/MinSizeSubarraySum.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+/*
+Sliding window technique: https://www.techinterviewhandbook.org/algorithms/array/#sliding-window
+Problem: https://leetcode.com/problems/minimum-size-subarray-sum/description/
+*/
+
+public class MinSizeSubarraySum {
+    public int minSubArrayLen(int target, int[] nums) {
+        int left = 0, sum = 0;
+        int result = Integer.MAX_VALUE;
+        for(int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+            while(sum >= target) {
+                result = Math.min(result, right - left + 1);
+                sum -= nums[left++];
+            }
+        }
+        return result == Integer.MAX_VALUE ? 0 : result;
+    }
+
+    public static void main(String[] args) {
+        MinSizeSubarraySum minSum = new MinSizeSubarraySum();
+        int minRes = minSum.minSubArrayLen(7, new int[]{2,3,1,2,4,3});
+        System.out.println(minRes);
+    }
+}
